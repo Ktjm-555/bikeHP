820a1d172f126c8e7fe08412dc73f7fc9ced304e
華帆 <mkaho0919kaho.wk@gmail.com>
2022-11-09 07:53:12 +0900

    環境構築完了（scss）

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<script setup>
+console.log('hello script setup')
+
+
+</script>
+<template>
+<div>
+  <h1>Hallo</h1>
+  <h2>aaa</h2>
+</div>
+</template>
+<style lang="scss">
+div {
+  h1 {
+    color: red;
+  }
+}
+</style>
+
