feb3567027a29030fa3b357609e4187a9da2cf8a
華帆 <mkaho0919kaho.wk@gmail.com>
2022-11-09 23:30:50 +0900

    とりあえず、大枠作った。

--- pages/index.vue
+++ pages/index.vue
@@ -5,15 +5,84 @@ console.log('hello script setup')
 </script>
 <template>
 <div>
-  <h1>Hallo</h1>
-  <h2>aaa</h2>
+  <header>
+
+  </header>
+  <div class="image_area">
+
+  </div>
+  <div class="about_area">
+
+    <div class="about_title">
+
+    </div>
+    <div class="about_detail">
+      
+    </div>
+  </div>
+    
+  <div class="bicycle_area">
+    <div class="bicycle_title">
+
+    </div>
+    <div class="bicycle_detail">
+
+    </div>
+  </div>
+
+  <footer>
+  </footer>
+
 </div>
 </template>
 <style lang="scss">
 div {
-  h1 {
-    color: red;
+
+  header {
+    height: 60px;
+    background-color: red;
   }
+
+  .image_area {
+    height: 600px;
+    background-color:aqua;
+  }
+
+  .about_area {
+    height: 395px;
+    background-color: greenyellow;
+
+    .about_title {
+      height: 198px;
+      background-color: blue;
+    }
+
+    .about_detail {
+
+    }
+  }
+
+  .bicycle_area {
+    height: 457px;
+    background-color: yellow;
+
+    .bicycle_title {
+      height: 48px;
+      background-color: red;
+    }
+
+    .bicycle_detail {
+      background-color: beige;
+    }
+  }
+
+  footer {
+    height: 20px;
+    background-color: blue;
+   
+  }
+
+
 }
 </style>
 
